007b0e82e5e9a0add3c3c35987feecc4bfcc4b98
Francisco Sanvicente Pico <64644109+FranciscoSanvicente@users.noreply.github.com>
2023-03-28 09:34:32 -0400

    Feat: design new documentation

---
+++ documentation/modules/components/nav/code/ts/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export /*bundle*/ function Nav() {
+    return (
+        <nav className="header">
+            <div className='nav-doc'>
+                <h1 className="logo">bgroup/ui <span className="logo__thin">Doc</span></h1>
+                <ul className="menu">
+                    <div className="menu__item toggle"><span></span></div>
+                    <li className="menu__item"><a href="" className="link link--dark"><i className="fa fa-github"></i> Github</a></li>
+                    <li className="menu__item"><a href="/doc" className="link link--dark"><i className="fa fa-home"></i> Documentación</a></li>
+                    <li className="menu__item"><a href="" className="link link--dark"><i className="fa fa-home"></i> Home</a></li>
+                </ul>
+            </div>
+
+        </nav>
+    )
+}
---
+++ documentation/modules/doc/ts/view/index.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import { Nav } from "@bgroup/documentation-ui/nav";
+import { CopyableImplementation } from "@bgroup/documentation-ui/copyable-implementation";
+export function Page() {
+  return (
+    <>
+      <div className="doc__bg"></div>
+      <Nav />
+      <div className="wrapper">
+        <aside className="doc__nav">
+          <ul>
+            <li className="js-btn selected">Get Started</li>
+            <li className="js-btn">Configuration</li>
+            <li className="js-btn">Keybindings</li>
+            <li className="js-btn">Issues</li>
+          </ul>
+        </aside>
+        <article className="doc__content">
+          <section className="js-section">
+            <h3 className="section__title">Get Started</h3>
+            <p>
+              Learn how to configure settings for bgroup/ui, such as your syntax
+              highlighting preference and the default saving folder location.
+            </p>
+            <h3 className="section__title">Installation</h3>
+            <div className="code__block code__block--notabs">
+              <pre className="code code--block">
+                <CopyableImplementation>
+                  {`npm i @bgroup/ui `}
+                </CopyableImplementation>
+              </pre>
+            </div>
+          </section>
+
+          <section className="js-section">
+            <h3 className="section__title">Keybindings</h3>
+            <p>
+              Lorem ipsum dolor sit amet, scripta tibique indoctum sit ei, mel
+              inani aeterno ad. Facer oratio ex per. At eam movet verear, in sea
+              brute patrioque disputando, usu nonumes torquatos an. Ex his
+              quaerendum intellegebat, ut vel homero accusam. Eum at debet
+              tibique, in vocibus temporibus adversarium sed. Porro verear eu
+              vix, ne usu tation vituperata.
+            </p>
+            <p>
+              Malis percipitur an pro. Pro aperiam persequeris at, at sonet
+              sensibus mei, id mea postulant definiebas concludaturque. Id qui
+              malis abhorreant, mazim melius quo et. At eam altera dolorum, case
+              dicant lobortis ius te, ad vel affert oportere reprehendunt. Quo
+              no verterem deseruisse, mea brute postea te, ne per tacimates
+              suavitate vituperatoribus.
+            </p>
+            <p>
+              Malis percipitur an pro. Pro aperiam persequeris at, at sonet
+              sensibus mei, id mea postulant definiebas concludaturque. Id qui
+              malis abhorreant, mazim melius quo et. At eam altera dolorum, case
+              dicant lobortis ius te, ad vel affert oportere reprehendunt. Quo
+              no verterem deseruisse, mea brute postea te, ne per tacimates
+              suavitate vituperatoribus.
+            </p>
+            <hr />
+          </section>
+          <section className="js-section">
+            <h3 className="section__title">Issues</h3>
+            <p>
+              Lorem ipsum dolor sit amet, scripta tibique indoctum sit ei, mel
+              inani aeterno ad. Facer oratio ex per. At eam movet verear, in sea
+              brute patrioque disputando, usu nonumes torquatos an. Ex his
+              quaerendum intellegebat, ut vel homero accusam. Eum at debet
+              tibique, in vocibus temporibus adversarium sed. Porro verear eu
+              vix, ne usu tation vituperata.
+            </p>
+            <p>
+              Malis percipitur an pro. Pro aperiam persequeris at, at sonet
+              sensibus mei, id mea postulant definiebas concludaturque. Id qui
+              malis abhorreant, mazim melius quo et. At eam altera dolorum, case
+              dicant lobortis ius te, ad vel affert oportere reprehendunt. Quo
+              no verterem deseruisse, mea brute postea te, ne per tacimates
+              suavitate vituperatoribus.
+            </p>
+          </section>
+        </article>
+      </div>
+
+      <footer className="footer">bgroup/ui ©Copyright </footer>
+    </>
+  );
+}
