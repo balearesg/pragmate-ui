6a4c740500e98173b7a4a2a1b22a96b6e504e487
Gabriel Castro <gaboj0531@gmail.com>
2023-05-20 20:33:26 -0400

    fix : new directory trash add and layout folder old in new documentation now

---
+++ documentation/modules/layouts/main/ts/bg-components.ts
@@ -0,0 +1,123 @@
+export const components = [
+  {
+    name: "Empty",
+    path: "empty",
+    subComponents: [],
+  },
+  {
+    name: "Form",
+    path: "form",
+    subComponents: [
+      {
+        name: "Checkbox",
+        path: "checkbox",
+      },
+      {
+        name: "Input",
+        path: "input",
+      },
+      {
+        name: "Switch",
+        path: "switch",
+      },
+      {
+        name: "Textarea",
+        path: "textarea",
+      },
+      {
+        name: "Button",
+        path: "button",
+      },
+      {
+        name: "Radio",
+        path: "radio",
+      },
+      {
+        name: "Form",
+        path: "form",
+      },
+    ],
+  },
+
+  {
+    name: "Modals",
+    path: "modals",
+    subComponents: [
+      {
+        name: "Alert",
+        path: "alert-modal",
+      },
+      {
+        name: "Confirm",
+        path: "confirm-modal",
+      },
+      {
+        name: "Regular",
+        path: "regular-modal",
+      },
+    ],
+  },
+
+  {
+    name: "Icon",
+    path: "icons",
+    subComponents: [],
+  },
+  {
+    name: "Spinner",
+    path: "spinner",
+    subComponents: [],
+  },
+  {
+    name: "Input Controller",
+    path: "input-controller",
+    subComponents: [],
+  },
+  {
+    name: "Image",
+    path: "image",
+    subComponents: [],
+  },
+
+  {
+    name: "Scroll",
+    path: "scroll-container",
+    subComponents: [],
+  },
+
+  {
+    name: "Tabs",
+    path: "tabs",
+    subComponents: [],
+  },
+  {
+    name: "Toast",
+    path: "toast",
+    subComponents: [],
+  },
+  {
+    name: "Swiper",
+    path: "swiper",
+    subComponents: [],
+  },
+  {
+    name: "Verification Code",
+    path: "code-verification",
+    subComponents: [],
+  },
+  {
+    name: "Alert",
+    path: "alert",
+    subComponents: [],
+  },
+  {
+    name: "Drag And Drop",
+    path: "drag-and-drop",
+    subComponents: [],
+  },
+  {
+    name: "Link",
+    path: "link",
+    subComponents: [],
+  },
+];
