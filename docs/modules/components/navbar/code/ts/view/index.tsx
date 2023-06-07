73e791a0a71a47ad5d6303b0223fa6f55160cfdc
Gabriel Castro <gaboj0531@gmail.com>
2023-06-07 10:37:51 -0400

    feat: added position sticky in navigation with effect scroll

--- docs/modules/components/navbar/code/ts/view/index.tsx
+++ docs/modules/components/navbar/code/ts/view/index.tsx
@@ -3,8 +3,26 @@ import { Link } from "pragmate-ui/link";
 import { ThemeSwitcher } from "pragmate-ui/theme-switcher";
 
 export /* bundle */ function Navbar() {
+  const [scrollPosition, setScrollPosition] = React.useState(0);
+
+  const handleScroll = () => {
+    setScrollPosition(window.pageYOffset);
+  };
+
+  React.useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const isScrolled = scrollPosition > 20;
   return (
-    <nav className="container__navbar">
+    <nav
+      className={`container__navbar ${
+        isScrolled ? "container__navbar-scrolled" : ""
+      }`}
+    >
       <h2 className="navbar__h2">
         <Link href="/">
           Pragmate <strong className="navbar__strong-hover">UI</strong>
