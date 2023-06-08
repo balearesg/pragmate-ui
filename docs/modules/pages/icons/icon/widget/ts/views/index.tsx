340b1c4d0a8bb1b52e1600e349a4eccee55848c0
Gabriel Castro <gaboj0531@gmail.com>
2023-06-08 15:08:58 -0400

    style: resolve component code  with in icon

--- docs/modules/pages/icons/icon/widget/ts/views/index.tsx
+++ docs/modules/pages/icons/icon/widget/ts/views/index.tsx
@@ -28,17 +28,19 @@ function View(): JSX.Element {
         <Code>{`import { Icon } from 'pragmate-ui/icons';`}</Code>
         <div className="component-implementation">
           <h3>Implementation:</h3>
-          <Code>{`<Icon icon="delete" />`}</Code>
-          <Code>{`
+          <Code className="component-implementation__code">{`<Icon icon="delete" />`}</Code>
+          <Code className="component-implementation__code">{`
            const users = {
             viewBox: "",
-            icon: '<path d="M3 5v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2H5c-1.11 0-2 .9-2 2zm12 4c0 1.66-1.34 3-3 3s-3-1.34-3-3 1.34-3 3-3 3 1.34 3 3zm-9 8c0-2 4-3.1 6-3.1s6 1.1 6 3.1v1H6v-1z"></path>',
+            icon: '<path d="M3 5v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2H5c-1.11 0-2 .9-2 2zm12 4c0
+             1.66-1.34 3-3 3s-3-1.34-3-3 1.34-3 3-3 3 1.34 3 3zm-9 8c0-2 4-3.1 6-3.1s6 1.1 6 3.1v1H6v-1z"></path>',
             };
             <Icon icon={users.icon} viewBox={users.viewBox} />`}</Code>
-          <Code>{`
+          <Code className="component-implementation__code">{`
            const users = {
             viewBox: "",
-            icon: '<path d="M3 5v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2H5c-1.11 0-2 .9-2 2zm12 4c0 1.66-1.34 3-3 3s-3-1.34-3-3 1.34-3 3-3 3 1.34 3 3zm-9 8c0-2 4-3.1 6-3.1s6 1.1 6 3.1v1H6v-1z"></path>',
+            icon: '<path d="M3 5v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2H5c-1.11 0-2 .9-2 2zm12 4c0 1.66-1.34 3-3 3s-3-1
+            .34-3-3 1.34-3 3-3 3 1.34 3 3zm-9 8c0-2 4-3.1 6-3.1s6 1.1 6 3.1v1H6v-1z"></path>',
             };
             `}</Code>
           <main className="component-container">
