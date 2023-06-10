a6bcfb7ef3b68571e8c988b321a7b0f5bf05c2b8
Gabriel Castro <gaboj0531@gmail.com>
2023-06-10 12:07:53 -0400

    style: ajust button copy in component code

--- docs/modules/pages/icons/icon-button/widget/ts/views/index.tsx
+++ docs/modules/pages/icons/icon-button/widget/ts/views/index.tsx
@@ -28,13 +28,12 @@ function View(): JSX.Element {
         <div className="component-implementation icon-button">
           <h3>Implementation:</h3>
           <Code>{`<IconButton icon="search" />`}</Code>
-          <Code>{`
-           const users = {
-            viewBox: "",
-            icon: '<path d="M3 5v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2H5c-1.11 0-2 .9-2 2zm12 4c0 1.66-1.34 3
-            -3 3s-3-1.34-3-3 1.34-3 3-3 3 1.34 3 3zm-9 8c0-2 4-3.1 6-3.1s6 1.1 6 3.1v1H6v-1z"></path>',
-            };
-            <IconButton icon={users.icon} viewBox={users.viewBox} />
+          <Code>{`const users = {
+    viewBox: "",
+    icon: '<path d="M3 5v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2H5c-1.11 0-2 .9-2 2zm12 4c0 1.66-1.34 3
+    -3 3s-3-1.34-3-3 1.34-3 3-3 3 1.34 3 3zm-9 8c0-2 4-3.1 6-3.1s6 1.1 6 3.1v1H6v-1z"></path>',
+  };
+    <IconButton icon={users.icon} viewBox={users.viewBox} />
             `}</Code>
           <main className="component-container">
             <h3>Result:</h3>
