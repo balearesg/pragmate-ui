4c4a8b07fdb0287d57c9d0ee575e77f0feaddfb7
Julio Rodriguez <jircdev@gmail.com>
2023-06-14 08:08:26 -0300

    feat: Add styles to icon buttons

--- project/modules/form/code/ts/form.tsx
+++ project/modules/form/code/ts/form.tsx
@@ -1,17 +1,17 @@
-import React, { FormHTMLAttributes, FormEvent, PropsWithChildren } from "react";
+import React, { FormHTMLAttributes, FormEvent, PropsWithChildren } from 'react';
 
 type props = FormHTMLAttributes<HTMLFormElement>;
 export /*bundle*/ function Form(props: PropsWithChildren<props>): JSX.Element {
-  const onSubmit = (event: FormEvent<HTMLFormElement>): void => {
-    event.preventDefault();
-    event.stopPropagation();
-    props.onSubmit(event);
-  };
-  const properties: props = Object.assign({}, props);
-  delete properties.onSubmit;
-  return (
-    <form onSubmit={onSubmit} {...properties}>
-      {props.children}
-    </form>
-  );
+	const { onSubmit, ...restProps } = props;
+	const handleSubmit = (event: FormEvent<HTMLFormElement>): void => {
+		event.preventDefault();
+		event.stopPropagation();
+		props.onSubmit(event);
+	};
+
+	return (
+		<form onSubmit={handleSubmit} {...restProps}>
+			{props.children}
+		</form>
+	);
 }
--- project/modules/spinner/code/ts/spinner.tsx
+++ project/modules/spinner/code/ts/spinner.tsx
@@ -1,45 +1,31 @@
-import React, {
-  ForwardRefExoticComponent,
-  forwardRef,
-  LegacyRef,
-  RefAttributes,
-} from "react";
+import React, { ForwardRefExoticComponent, forwardRef, LegacyRef, RefAttributes } from 'react';
 type properties = {
-  color?: string;
-  className?: string;
-  type: string;
-  size?: string;
-  active: boolean;
+	color?: string;
+	className?: string;
+	type: string;
+	size?: string;
+	active: boolean;
 };
 export /*bundle*/
-const Spinner: ForwardRefExoticComponent<properties & RefAttributes<unknown>> =
-  forwardRef(
-    (props: properties, ref: LegacyRef<HTMLDivElement>): JSX.Element => {
-      let { color, className, size, type, active } = props;
-      const types = [
-        "on-primary",
-        "on-secondary",
-        "on-surface",
-        "on-error",
-        "primary",
-        "secondary",
-        "tertiary",
-      ];
-      const clsType = types.includes(type) ? type : "primary";
-      console.log(10, type, clsType);
-      const style: { stroke?: string } = {};
-      if (color) style.stroke = color;
-      className = `${clsType}${className ? ` ${className}` : ""}`;
-      let cls: string = className
-        ? `${className} pragmate-element-spinner`
-        : "pragmate-element-spinner";
-      if (active) cls += " is-active";
-      return (
-        <div className={cls} ref={ref}>
-          <svg viewBox="0 0 100 100">
-            <circle cx="50" cy="50" r="20" style={style} />
-          </svg>
-        </div>
-      );
-    }
-  );
+const Spinner: ForwardRefExoticComponent<properties & RefAttributes<unknown>> = forwardRef(
+	(props: properties, ref: LegacyRef<HTMLDivElement>): JSX.Element => {
+		let { color, className, size, type, active } = props;
+
+		const types = ['on-primary', 'on-secondary', 'on-surface', 'on-error', 'primary', 'secondary', 'tertiary'];
+		const clsType = types.includes(type) ? type : 'primary';
+
+		const style: { stroke?: string } = {};
+		if (color) style.stroke = color;
+		className = `${clsType}${className ? ` ${className}` : ''}`;
+		let cls: string = className ? `${className} pragmate-element-spinner` : 'pragmate-element-spinner';
+		if (active) cls += ' is-active';
+
+		return (
+			<div className={cls} ref={ref}>
+				<svg viewBox="0 0 100 100">
+					<circle cx="50" cy="50" r="20" style={style} />
+				</svg>
+			</div>
+		);
+	}
+);
