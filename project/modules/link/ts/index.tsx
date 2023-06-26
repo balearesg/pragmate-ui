ba2081c4993768a3ddeef4c44f12ee0d549598af
=Julio Rodriguez <jircdev@gmail.com>
2023-06-26 13:33:03 -0300

    chore: remove consoles

--- project/modules/link/ts/index.tsx
+++ project/modules/link/ts/index.tsx
@@ -1,48 +1,32 @@
-import * as React from "react";
-import { routing } from "@beyond-js/kernel/routing";
-
-
+import * as React from 'react';
+import { routing } from '@beyond-js/kernel/routing';
 
 export /*bundle*/
-function Link({href, ...props}: React.AnchorHTMLAttributes<HTMLAnchorElement>): JSX.Element {
-console.log(href)
-    const isExternal = href.startsWith("http");
-  const target = isExternal ? '_blank' : '';
-  
-  const onClick = (event: React.MouseEvent<HTMLAnchorElement>): void => {
-    if (!isExternal) {
-       event.preventDefault()
-    };
-    
-    event.stopPropagation();
+function Link({ href, ...props }: React.AnchorHTMLAttributes<HTMLAnchorElement>): JSX.Element {
+	const isExternal = href.startsWith('http');
+	const target = isExternal ? '_blank' : '';
 
-    if (props.onClick && typeof props.onClick === "function") {
-      props.onClick(event);
-    }
+	const onClick = (event: React.MouseEvent<HTMLAnchorElement>): void => {
+		if (!isExternal) {
+			event.preventDefault();
+		}
 
-    !isExternal && routing.pushState(href);
-  };
+		event.stopPropagation();
 
-  const cls = props.className || ''
-   const properties = { ...props };
-  ["href", "onClick"].forEach((prop) => delete properties[prop]);
+		if (props.onClick && typeof props.onClick === 'function') {
+			props.onClick(event);
+		}
 
-  return (
-    <a {...properties} href={href} onClick={onClick} target={props.target || target} className={`tag-a ${cls}`}>
-      {props.children}
-    </a>
-  );
+		!isExternal && routing.pushState(href);
+	};
+
+	const cls = props.className || '';
+	const properties = { ...props };
+	['href', 'onClick'].forEach(prop => delete properties[prop]);
+
+	return (
+		<a {...properties} href={href} onClick={onClick} target={props.target || target} className={`tag-a ${cls}`}>
+			{props.children}
+		</a>
+	);
 }
-
-// export /*bundle*/
-// function ELink(
-//   props: React.AnchorHTMLAttributes<HTMLAnchorElement>
-// ): JSX.Element {
-//   const { href } = props;
-//   delete props.href;
-//   return (
-//     <a href={href} target="_blank" {...props}>
-//       {props.children}
-//     </a>
-//   );
-// }
