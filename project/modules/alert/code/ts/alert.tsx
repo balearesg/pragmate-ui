0d13e5fed2119bd3e95a18484fde9475dd36135e
=Julio Rodriguez <jircdev@gmail.com>
2023-07-05 20:18:18 -0300

    feat: Works

--- project/modules/alert/code/ts/alert.tsx
+++ project/modules/alert/code/ts/alert.tsx
@@ -1,58 +1,55 @@
-import React from "react";
-import { Icon, IconButton } from "pragmate-ui/icons";
+import React from 'react';
+import { Icon, IconButton } from 'pragmate-ui/icons';
 
-type props = {
-  show?: boolean;
-  className?: string;
-  mode?: string;
-  message?: string;
-  title?: string;
-  isClose?: boolean;
+type IProps = {
+	show?: boolean;
+	className?: string;
+	mode?: string;
+	message?: string;
+	title?: string;
+	isClose?: boolean;
+	children?: React.ReactNode;
 };
 
 type iconMap = {
-  [key: string]: string;
+	[key: string]: string;
 };
 
 export /*bundle*/
-function Alert(props: props) {
-  const { message, show, className, mode, title, isClose } = props;
-  const [state, setState] = React.useState({
-    show: show ?? false,
-    text: message ?? "texto de la alerta",
-  });
-  React.useEffect(() => {
-    setState({ ...state, show: show });
-  }, [show]);
-  if (!state.show) return null;
-  const close = (): void => setState({ ...state, show: false, text: "" });
-  const cls: string = className
-    ? `${className} alert ${mode ?? "success"}`
-    : `alert ${mode ?? "success"}`;
+function Alert(props: IProps) {
+	const { message, show, className, mode, title, isClose, children } = props;
+	const [state, setState] = React.useState({
+		show: show ?? false,
+		text: message ?? 'texto de la alerta',
+	});
+	React.useEffect(() => {
+		setState({ ...state, show: show });
+	}, [show]);
+	if (!state.show) return null;
+	const close = (): void => setState({ ...state, show: false, text: '' });
+	const cls: string = className ? `${className} alert ${mode ?? 'success'}` : `alert ${mode ?? 'success'}`;
 
-  const icons: iconMap = {
-    error: "error",
-    warning: "circle-exclamation",
-    success: "circle-check",
-    info: "info",
-  };
+	const icons: iconMap = {
+		error: 'error',
+		warning: 'circle-exclamation',
+		success: 'circle-check',
+		info: 'info',
+	};
 
-  const defaulIcon = icons[mode ?? "success"];
+	const defaulIcon = icons[mode ?? 'success'];
 
-  return (
-    <div className={cls} onClick={close}>
-      {" "}
-      {title && (
-        <div className="alert__title">
-          <Icon icon={defaulIcon} />{" "}
-          <span
-            className="alert__title-span"
-            dangerouslySetInnerHTML={{ __html: title }}
-          />
-        </div>
-      )}
-      {message && <span dangerouslySetInnerHTML={{ __html: message }}></span>}
-      {isClose && <IconButton icon="close" onClick={close} />}
-    </div>
-  );
+	return (
+		<div className={cls} onClick={close}>
+			{' '}
+			{title && (
+				<div className='alert__title'>
+					<Icon icon={defaulIcon} />{' '}
+					<span className='alert__title-span' dangerouslySetInnerHTML={{ __html: title }} />
+				</div>
+			)}
+			{children}
+			{message && <span dangerouslySetInnerHTML={{ __html: message }}></span>}
+			{isClose && <IconButton icon='close' onClick={close} />}
+		</div>
+	);
 }
