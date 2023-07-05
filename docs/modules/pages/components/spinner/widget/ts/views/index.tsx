feb8135023620ec0239fb688d6a5b06811744564
Gabriel Castro <gaboj0531@gmail.com>
2023-07-05 15:09:36 -0400

    chore: I copied the components button and spinner and put them in the components folder in the library and corrected the description of their use in the documentation

--- docs/modules/pages/components/spinner/widget/ts/views/index.tsx
+++ docs/modules/pages/components/spinner/widget/ts/views/index.tsx
@@ -24,7 +24,7 @@ function View() {
       </p>
       <div>
         <h3>Import:</h3>
-        <Code>{`import { Spinner } from 'pragmate-ui/spinner';`}</Code>
+        <Code>{` import { Spinner } from 'pragmate-ui/components ' ;`}</Code>
         <main className="component-container">
           <div className="column">
             <h3>Colors:</h3>
