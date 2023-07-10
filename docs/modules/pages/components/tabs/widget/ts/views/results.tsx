11380491e89fb0694f8ae77bef6933b83cce346f
Gabriel Castro <gaboj0531@gmail.com>
2023-07-10 11:48:52 -0400

    style: fixed style in tabs component

--- docs/modules/pages/components/tabs/widget/ts/views/results.tsx
+++ docs/modules/pages/components/tabs/widget/ts/views/results.tsx
@@ -7,28 +7,16 @@ export function Results() {
   return (
     <TabsContainer selected={tab}>
       <Tabs selected={tab}>
-        <button
-          data-path="/components/beyond-tabs?tab=0"
-          onClick={() => setTab(0)}
-        >
+        <button data-path="/components/tabs?tab=0" onClick={() => setTab(0)}>
           TAB 0
         </button>
-        <button
-          data-path="/components/beyond-tabs?tab=1"
-          onClick={() => setTab(1)}
-        >
+        <button data-path="/components/tabs?tab=1" onClick={() => setTab(1)}>
           TAB 1
         </button>
-        <button
-          data-path="/components/beyond-tabs?tab=2"
-          onClick={() => setTab(2)}
-        >
+        <button data-path="/components/tabs?tab=2" onClick={() => setTab(2)}>
           TAB 2
         </button>
-        <button
-          data-path="/components/beyond-tabs?tab=3"
-          onClick={() => setTab(3)}
-        >
+        <button data-path="/components/tabs?tab=3" onClick={() => setTab(3)}>
           TAB 3
         </button>
       </Tabs>
