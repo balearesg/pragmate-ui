48212c926c2a1701bee1b7ba73de78520b278e60
Julio Rodriguez <jircdev@gmail.com>
2023-08-14 23:04:55 -0300

    chore: Refactor menu component

---
+++ docs/modules/layouts/main/ts/menu/form.ts
@@ -0,0 +1,10 @@
+const children = [
+	['Checkbox', 'checkbox'],
+	['Input', 'input'],
+	['Switch', 'switch'],
+	['Textarea', 'textarea'],
+	['Radio', 'radio'],
+	['Form', 'form'],
+];
+
+export const FormItems = ['Form', 'form', children];
