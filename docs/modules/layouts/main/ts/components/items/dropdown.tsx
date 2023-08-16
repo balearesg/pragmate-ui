c6438a541ff8729ffc25a89cbda73f469321b4c1
Gabriel Castro <gaboj0531@gmail.com>
2023-08-15 21:33:47 -0400

    chore: start add to mdx documentation

--- docs/modules/layouts/main/ts/components/items/dropdown.tsx
+++ docs/modules/layouts/main/ts/components/items/dropdown.tsx
@@ -1,37 +1,45 @@
-import * as React from 'react';
-import { SidebarItem } from './item';
-import { Icon } from 'pragmate-ui/icons';
-import { useBinder } from '@beyond-js/react-18-widgets/hooks';
-import { routing } from '@beyond-js/kernel/routing';
-import { IComponent, IItem } from '../interfaces/IComponent';
+import * as React from "react";
+import { SidebarItem } from "./item";
+import { Icon } from "pragmate-ui/icons";
+import { useBinder } from "@beyond-js/react-18-widgets/hooks";
+import { routing } from "@beyond-js/kernel/routing";
+import { IComponent, IItem } from "../interfaces/IComponent";
 
 export function SidebarDropdown({ component, tabIndex }) {
-	const [isDropdownOpen, setIsDropdownOpen] = React.useState(false);
-	const [currentRouting, setCurrentRouting] = React.useState(routing.uri.pathname);
-	const location = `/components/${component.path}`;
-	const isChecked = location === routing.uri.pathname ? 'sidebar__item--checked' : 'sidebar__item';
-	const iconDirection = isDropdownOpen ? 'down' : 'right';
-	const icon = isDropdownOpen ? 'expandMore' : 'chevronRight';
+  const [isDropdownOpen, setIsDropdownOpen] = React.useState(false);
+  const [currentRouting, setCurrentRouting] = React.useState(
+    routing.uri.pathname
+  );
+  const location = `/components/${component.name}`;
 
-	useBinder([routing], () => setCurrentRouting(routing.uri.pathname));
-	const [path, name, children] = component;
-	const subComponentsElements = children.map((subComponent: IItem) => {
-		const [path] = subComponent;
-		return <SidebarItem key={path} component={subComponent} tabIndex={tabIndex} />;
-	});
+  const isChecked =
+    location === routing.uri.pathname
+      ? "sidebar__item--checked"
+      : "sidebar__item";
+  const iconDirection = isDropdownOpen ? "down" : "right";
+  const icon = isDropdownOpen ? "expandMore" : "chevronRight";
 
-	function toggleDropdown(event) {
-		event.preventDefault();
-		setIsDropdownOpen(!isDropdownOpen);
-	}
+  useBinder([routing], () => setCurrentRouting(routing.uri.pathname));
+  const [path, name, children] = component;
+  const subComponentsElements = children.map((subComponent: IItem) => {
+    const [path] = subComponent;
+    return (
+      <SidebarItem key={path} component={subComponent} tabIndex={tabIndex} />
+    );
+  });
 
-	return (
-		<li className={isChecked}>
-			<a className='sidebar-item__icon' onClick={toggleDropdown}>
-				{name}
-				<Icon icon={icon} name={name} className={iconDirection} />
-			</a>
-			{isDropdownOpen && <ul className='sub-list'>{subComponentsElements}</ul>}
-		</li>
-	);
+  function toggleDropdown(event) {
+    event.preventDefault();
+    setIsDropdownOpen(!isDropdownOpen);
+  }
+
+  return (
+    <li className={isChecked}>
+      <a className="sidebar-item__icon" onClick={toggleDropdown}>
+        {name}
+        <Icon icon={icon} name={name} className={iconDirection} />
+      </a>
+      {isDropdownOpen && <ul className="sub-list">{subComponentsElements}</ul>}
+    </li>
+  );
 }
--- docs/modules/pages/components/alert/ts/views/index.tsx
+++ docs/modules/pages/components/alert/ts/views/index.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { View } from "./view";
+import Content from "./content.mdx";
+
 export /*bundle*/
 function Page() {
-  return <View />;
+  return <Content />;
 }
