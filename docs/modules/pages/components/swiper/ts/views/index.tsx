d02986058a3cf4a615fa60103bddb05342b1a621
Gabriel Castro <gaboj0531@gmail.com>
2023-08-20 12:29:59 -0400

    fix: other documentations with mdx uploads and unused folders removed

--- docs/modules/pages/components/swiper/ts/views/index.tsx
+++ docs/modules/pages/components/swiper/ts/views/index.tsx
@@ -1,38 +1,11 @@
 import * as React from "react";
-import { Result } from "./result";
-import { implementation } from "./implementation";
-import { Code } from "pragmate-ui/code";
-import { component } from "./component";
+import Content from "./content.mdx";
+
 export /*bundle*/
 function View() {
   return (
     <div className="view">
-      <h1 className="view__h1">Swiper</h1>
-      <p className="view__p">
-        El componente <span className="view__span">Swiper</span> es un
-        reproductor de slides interactivo y personalizable que se utiliza para
-        mostrar contenido en forma de diapositivas deslizables. El componente
-        Swiper es especialmente útil para aplicaciones web y de escritorio que
-        requieren presentar información de manera dinámica y atractiva.
-      </p>
-      <div className="component-info">
-        <h3>Import:</h3>
-        <Code>{`import { SwiperSlider } from "pragmate-ui/swiper-component"`}</Code>
-      </div>
-      <div className="component-implementation">
-        <div className="component">
-          <h3>component :</h3>
-          <Code>{component}</Code>
-        </div>
-        <div className="component-info">
-          <h3>Implementation:</h3>
-          <Code>{implementation}</Code>
-        </div>
-        <div className="component-result">
-          <h3>Result:</h3>
-          <Result />
-        </div>
-      </div>
+      <Content />
     </div>
   );
 }
--- docs/modules/pages/components/theme-switch/ts/view/index.tsx
+++ docs/modules/pages/components/theme-switch/ts/view/index.tsx
@@ -1,36 +1,11 @@
 import * as React from "react";
-import { implementation } from "./implementation";
-import { Results } from "./result";
-import { Code } from "pragmate-ui/code";
+import Content from "./content.mdx";
 
 export /*bundle*/
 function View() {
   return (
     <div className="view">
-      <h1>ThemeSwitch</h1>
-      <p>
-        Este componente Theme Switch es un elemento de la interfaz de usuario
-        que permite cambiar fácilmente entre temas claros y oscuros en una
-        aplicación o sistema operativo. Este componente puede ser especialmente
-        útil en aplicaciones web y de escritorio, así como en sistemas
-        operativos como Windows 10, que admiten temas claros y oscuros.
-        Funcionalidades Muestra un botón o interruptor para cambiar entre temas
-        claros y oscuros.
-      </p>
-      <div className="component-info">
-        <h3>Import:</h3>
-        <Code>{`import { ThemeSwicth } from "pragmate-ui/theme-switcher"`}</Code>
-      </div>
-      <div className="component-implementation">
-        <div className="component-info">
-          <h3>Implementation: </h3>
-          <Code>{implementation}</Code>
-        </div>
-        <div className="component-result">
-          <h3>Result:</h3>
-          <Results />
-        </div>
-      </div>
+      <Content />
     </div>
   );
 }
