13f801875da288d7511b373d3b9c0c9ff242c7f2
Gabriel Castro <gaboj0531@gmail.com>
2023-08-19 20:07:22 -0400

    chore: add others files mdx for components pages

--- docs/modules/pages/components/princings/ts/views/index.tsx
+++ docs/modules/pages/components/princings/ts/views/index.tsx
@@ -1,92 +1,10 @@
 import * as React from "react";
-import { Code } from "pragmate-ui/code";
-import { implementation } from "./implementation";
-import { Results } from "./result";
-import { ResultsPremium } from "./result-premium";
-import { ResultsFree } from "./result-free";
-import { component } from "./component";
+import Content from "./content.mdx";
 export /*bundle*/
 function View() {
   return (
     <div className="view view-pricing">
-      <h1 className="view__h1">Pricing</h1>
-      <p className="view__p">
-        El componente <span className="view__span">Pricing</span> es un
-        componente de React que permite mostrar diferentes planes de precios en
-        una página web o aplicación. Este componente acepta varias props que
-        permiten personalizar el título, el precio, la descripción y las
-        funciones adicionales de cada plan de precio.
-      </p>
-
-      <div className="component-info">
-        <h3>Import: </h3>
-        <Code>{`import { Pricing } from 'pragmate-ui/pricing';`}</Code>
-      </div>
-
-      <div className="component-implementation">
-        <div className="component">
-          <h3>component :</h3>
-          <Code>{component}</Code>
-        </div>
-        <div className="component-info">
-          <h3>Implementation: </h3>
-          <Code>{implementation}</Code>
-        </div>
-        <div className="component-result component-result-precing">
-          <span>
-            <h3>Result:</h3>
-            <Results />
-          </span>
-          <div className="container__p-pricing">
-            <h3 className="view__h3-props">Props :</h3>
-            <ul>
-              <li>
-                <strong>Variant :</strong> variante del pricing que desea
-                asignar, "free" o "premium".
-              </li>
-              <li>
-                <strong>Title :</strong> titulo del pricing.
-              </li>
-              <li>
-                <strong>Description :</strong> descripcion del pricing.
-              </li>
-              <li>
-                <strong>Price :</strong> precio que desee indicar al pricing
-                (esta propiedad solo es requerida en la variante "premium").
-              </li>
-              <li>
-                <strong>Advantages :</strong> lista de los beneficios de su
-                pricing, esta propiedad recibe un arreglo donde estara los
-                beneficios.
-              </li>
-            </ul>
-            <p className="view__p">
-              los <strong>pricing</strong> proporciona valores predeterminados
-              para cada prop en caso de que no se proporcionen explícitamente.
-              Esto hace que sea más fácil para el usuario personalizar solo las
-              props que desea, sin tener que preocuparse por proporcionar todas
-              las props necesarias.
-            </p>
-          </div>
-        </div>
-      </div>
-      <h3>Variant:</h3>
-      <p className="view__p p-pricing">
-        El componente <strong>Pricing</strong> utiliza otros componentes de
-        variantes de precio (FreePlan y PremiumPlan) para renderizar la
-        información correspondiente a cada plan de precio. El nivel de precio se
-        especifica mediante la prop <strong>"Variant"</strong>:
-      </p>
-      <div className="content-result">
-        <div className="result">
-          <h4>Variant Free</h4>
-          <ResultsFree />
-        </div>
-        <div className="result">
-          <h4>Variant Premiun</h4>
-          <ResultsPremium />
-        </div>
-      </div>
+      <Content />
     </div>
   );
 }
---
+++ project/modules/breadcrumb/code/ts/index.tsx
@@ -0,0 +1,51 @@
+import * as React from "react";
+import { Link } from "pragmate-ui/link";
+import { routing } from "@beyond-js/kernel/routing";
+import { IProps } from "./types";
+import { useBinder } from "@beyond-js/react-18-widgets/hooks";
+
+export /* bundle */ function BreadCrumb({
+  title,
+  items,
+  border,
+  className,
+  children,
+}) {
+  let cls = `breadcrumb-page-header${className ? ` ${className}` : ""}`;
+  if (border) cls += " border-header";
+
+  const [currentRouting, setCurrentRouting] = React.useState(
+    routing.uri.pathname
+  );
+
+  useBinder([routing], () => setCurrentRouting(routing.uri.pathname));
+
+  let breadcrumbOutput = [];
+
+  if (items) {
+    breadcrumbOutput = items.map(([link, label]) => {
+      const isChecked =
+        link === routing.uri.pathname
+          ? "breadcrumbs__item--checked"
+          : "breadcrumbs__item";
+
+      return (
+        <li key={`${link} ${label}`}>
+          <Link className={isChecked} href={link}>
+            {label}
+          </Link>
+        </li>
+      );
+    });
+  }
+
+  return (
+    <header className={cls}>
+      <h2>{title}</h2>
+      {breadcrumbOutput.length > 0 && (
+        <ul className="breadcrumb">{breadcrumbOutput}</ul>
+      )}
+      <div>{children}</div>
+    </header>
+  );
+}
