4c852470f8e253f32350b1c869bb7131b7d87aa5
Gabriel Castro <gaboj0531@gmail.com>
2023-08-24 16:08:01 -0400

    feat: add page documentation for tooltips in docs

---
+++ docs/modules/components/tooltips/ts/views/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import Content from "./content.mdx";
+import { Code } from "pragmate-ui/code";
+import { IconButton } from "pragmate-ui/icons";
+import { Button } from "pragmate-ui/components";
+
+export /*bundle*/
+function View() {
+  return (
+    <div className="view">
+      <Content />
+    </div>
+  );
+}
--- docs/modules/layouts/main/ts/menu/index.ts
+++ docs/modules/layouts/main/ts/menu/index.ts
@@ -1,75 +1,76 @@
-import { FormItems } from './form';
+import { FormItems } from "./form";
 
 const modalsChildren = [
-	['Alert', 'alert-modal'],
-	['Confirm', 'confirm-modal'],
-	['Regular', 'regular-modal'],
+  ["Alert", "alert-modal"],
+  ["Confirm", "confirm-modal"],
+  ["Regular", "regular-modal"],
 ];
 
 const iconsChildren = [
-	['Icon', 'icons-icon'],
-	['Icon Button', 'icon-button'],
-	['Icon List', 'icon-list'],
+  ["Icon", "icons-icon"],
+  ["Icon Button", "icon-button"],
+  ["Icon List", "icon-list"],
 ];
 
 const menuChildren = [
-	['Menu', 'menu'],
-	['DropdownBottomMenu', 'dropdown-bottom-menu'],
+  ["Menu", "menu"],
+  ["DropdownBottomMenu", "dropdown-bottom-menu"],
 ];
 
 // ... Las demás variables para los children aquí ...
 
 export const menuItems = [
-	['Empty', 'empty', []],
-	FormItems,
-	[
-		'Modals',
-		'modals',
-		[
-			['Alert', 'alert-modal'],
-			['Confirm', 'confirm-modal'],
-			['Regular', 'regular-modal'],
-		],
-	],
-	[
-		'Icons',
-		'icons',
-		[
-			['Icon', 'icons-icon'],
-			['Icon Button', 'icon-button'],
-			['Icon List', 'icon-list'],
-		],
-	],
-	[
-		'Menu',
-		'menu',
-		[
-			['Menu', 'menu'],
-			['DropdownBottomMenu', 'dropdown-bottom-menu'],
-		],
-	],
-	['Theme-switch', 'theme-switch', []],
-	['Button', 'buttons', []],
-	['Code', 'code', []],
-	['Pricing', 'pricing', []],
-	['Spinner', 'spinner', []],
-	['BreadCrumb', 'breadcrumb', []],
-	['Opinions', 'opinions', []],
-	['Input Controller', 'input-controller', []],
-	['Image', 'image', []],
-	['Scroll', 'scroll-container', []],
-	['ButtonGroup', 'button-group'],
-	['Tabs', 'tabs', []],
-	['New Tabs', 'tabs-new', []],
-	['Toast', 'toast', []],
-	['Swiper', 'swiper', []],
-	['Verification Code', 'code-verification', []],
-	['Select', 'select', []],
-	['Alert', 'alert', []],
-	['Drag And Drop', 'drag-and-drop', []],
-	['Link', 'link', []],
+  ["Empty", "empty", []],
+  FormItems,
+  [
+    "Modals",
+    "modals",
+    [
+      ["Alert", "alert-modal"],
+      ["Confirm", "confirm-modal"],
+      ["Regular", "regular-modal"],
+    ],
+  ],
+  [
+    "Icons",
+    "icons",
+    [
+      ["Icon", "icons-icon"],
+      ["Icon Button", "icon-button"],
+      ["Icon List", "icon-list"],
+    ],
+  ],
+  [
+    "Menu",
+    "menu",
+    [
+      ["Menu", "menu"],
+      ["DropdownBottomMenu", "dropdown-bottom-menu"],
+    ],
+  ],
+  ["Theme-switch", "theme-switch", []],
+  ["Button", "buttons", []],
+  ["Code", "code", []],
+  ["Pricing", "pricing", []],
+  ["Spinner", "spinner", []],
+  ["BreadCrumb", "breadcrumb", []],
+  ["Opinions", "opinions", []],
+  ["Input Controller", "input-controller", []],
+  ["Image", "image", []],
+  ["Scroll", "scroll-container", []],
+  ["ButtonGroup", "button-group"],
+  ["Tabs", "tabs", []],
+  ["New Tabs", "tabs-new", []],
+  ["Toast", "toast", []],
+  ["Swiper", "swiper", []],
+  ["Verification Code", "code-verification", []],
+  ["Select", "select", []],
+  ["Alert", "alert", []],
+  ["Tooltips", "tooltips", []],
+  ["Drag And Drop", "drag-and-drop", []],
+  ["Link", "link", []],
 ].sort((a, b) => {
-	if (a[1] < b[1]) return -1;
-	if (a[1] > b[1]) return 1;
-	return 0;
+  if (a[1] < b[1]) return -1;
+  if (a[1] > b[1]) return 1;
+  return 0;
 });
