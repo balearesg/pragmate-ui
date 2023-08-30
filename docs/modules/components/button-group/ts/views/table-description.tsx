7f7ba1da8b4ebd50cc6f0b58d7a915b876a55ed0
Gabriel Castro <gaboj0531@gmail.com>
2023-08-29 21:31:26 -0400

    feat:buttonGroup component improvement of its styles and attributes that can be used to use it

--- docs/modules/components/button-group/ts/views/table-description.tsx
+++ docs/modules/components/button-group/ts/views/table-description.tsx
@@ -13,13 +13,6 @@ export function TableDescription() {
 
       <tbody>
         <tr>
-          <td>variant</td>
-          <td>Establece el estilo visual del grupo de botones</td>
-          <td>string</td>
-          <td>primary</td>
-        </tr>
-
-        <tr>
           <td>size</td>
           <td>Tamaño de los botones</td>
           <td>small, medium, tall</td>
@@ -31,20 +24,6 @@ export function TableDescription() {
           <td>Orientación de los botones, en fila o columna</td>
           <td>row, column</td>
           <td>row</td>
-        </tr>
-
-        <tr>
-          <td>bordered</td>
-          <td>Agrega borde alrededor del grupo</td>
-          <td>boolean</td>
-          <td>false</td>
-        </tr>
-
-        <tr>
-          <td>children</td>
-          <td>Los elementos Button que se mostrarán dentro del grupo</td>
-          <td>ReactNode</td>
-          <td>-</td>
         </tr>
       </tbody>
     </table>
---
+++ project/modules/components/ts/button-group/type.ts
@@ -0,0 +1,6 @@
+export interface IProps {
+  variant?: string;
+  size?: "small" | "medium" | "tall";
+  orientation?: "row" | "column";
+  children: JSX.Element;
+}
