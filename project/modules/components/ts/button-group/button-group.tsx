2ee9cb55d4be8507a3e3d04d6c506ae02be1ecd3
Gabriel Castro <gaboj0531@gmail.com>
2023-08-31 11:28:43 -0400

    fix: resolve observations for the pull requests

--- project/modules/components/ts/button-group/button-group.tsx
+++ project/modules/components/ts/button-group/button-group.tsx
@@ -1,20 +1,15 @@
-import * as React from "react";
-import type { IProps } from "./type";
+import * as React from 'react';
+import type {IProps} from './type';
 export /* bundle */ function ButtonGroup(props: IProps): JSX.Element {
-  const {
-    variant = "primary",
-    size = "medium",
-    orientation = "row",
-    children,
-  } = props;
+	const {orientation = 'row', children} = props;
 
-  let cls = `pragmate-button-group`;
-  cls += orientation ? ` pragmate-button-group ${orientation}` : "";
-  cls += size ? `${cls} ${size}` : "";
+	let cls = `pragmate-button-group`;
+	cls += orientation ? ` pragmate-button-group ${orientation}` : '';
+	/* cls += size ? `${cls} ${size}` : ""; */
 
-  return (
-    <div className="content-group">
-      <div className={cls}>{children}</div>
-    </div>
-  );
+	return (
+		<div className="content-group">
+			<div className={cls}>{children}</div>
+		</div>
+	);
 }
