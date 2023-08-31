21af97e03b83b60bed3ad1f96536132d50e6603b
Gabriel Castro <gaboj0531@gmail.com>
2023-08-31 13:28:52 -0400

    fix: config my file prettierrc

--- project/modules/components/ts/button-group/button-group.tsx
+++ project/modules/components/ts/button-group/button-group.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
 import type { IProps } from './type';
+
 export /* bundle */ function ButtonGroup(props: IProps): JSX.Element {
 	const { orientation = 'row', children } = props;
-
 	let cls = `pragmate-button-group`;
 	cls += orientation ? ` pragmate-button-group ${orientation}` : '';
-	/* cls += size ? `${cls} ${size}` : ""; */
-
 	return (
 		<div className="content-group">
 			<div className={cls}>{children}</div>
