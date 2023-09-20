a518738c7d009423911468d3fc4e662eea4ae0ab
veD-tnayrB <Brayanmc.contacto@gmail.com>
2023-09-20 08:31:03 -0400

    Fix: Solve the buttons variant

--- docs/modules/components/button/ts/views/button-border.tsx
+++ docs/modules/components/button/ts/views/button-border.tsx
@@ -5,9 +5,9 @@ export function ResultsBorder(): JSX.Element {
 	const buttons = ['primary', 'secondary', 'tertiary', 'success', 'warning', 'danger', 'info', 'link'];
 
 	const output = buttons.map(item => (
-		<Button key={item} variant={item} bordered={true} title={item}>
+		<Button key={item} variant={item} bordered title={item}>
 			variant="{item}"
 		</Button>
 	));
-	return <div className='content'>{output}</div>;
+	return <div className="content">{output}</div>;
 }
