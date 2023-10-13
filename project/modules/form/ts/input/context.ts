0622c7372f6cc45d220c9563c11b478e6ed44768
Gabriel Castro <gaboj0531@gmail.com>
2023-10-13 18:07:07 -0400

    chore: ajust interface of context in component input

--- project/modules/form/ts/input/context.ts
+++ project/modules/form/ts/input/context.ts
@@ -1,4 +1,28 @@
 import React from 'react';
+import type {IProps} from './type';
+import {MutableRefObject, InputHTMLAttributes, ReactNode} from 'react';
 
-export const InputContext = React.createContext({} as any);
+interface IInputContextValue {
+	state: {
+		value?: string | number | readonly string[];
+		errorMessage: string;
+		lengthMessage: string;
+		emptyMessage: string;
+		_hasError?: boolean;
+		type: InputHTMLAttributes<HTMLInputElement>['type'];
+	};
+	props: IProps;
+	setState: React.Dispatch<
+		React.SetStateAction<{
+			value: string;
+			errorMessage: string;
+			lengthMessage: string;
+			emptyMessage: string;
+			type: string;
+		}>
+	>;
+	input: MutableRefObject<HTMLInputElement>;
+}
+
+export const InputContext = React.createContext({} as IInputContextValue);
 export const useInputContext = () => React.useContext(InputContext);
