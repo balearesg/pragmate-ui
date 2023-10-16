1ff5e4f82d3fd66d4ca3b298b17c2a53028dc7a0
Gabriel Castro <gaboj0531@gmail.com>
2023-10-16 11:09:39 -0400

    feat: update documentation of vertical timeline component

--- docs/modules/components/vertical-line/ts/views/index.tsx
+++ docs/modules/components/vertical-line/ts/views/index.tsx
@@ -1,29 +1,13 @@
 import * as React from 'react';
 import {VerticalLine} from 'pragmate-ui/vertical-line';
+import {Code} from 'pragmate-ui/code';
+import Content from './content.mdx';
 
 export /*bundle*/
 function Page() {
-	const events = [
-		{date: '2m ago', description: 'Evento 1 con mas texto', title: 'Appointment', icon: 'doc'},
-		{date: 'Jul 10', description: 'Evento 2 con mas texto que el otro', title: 'Medication', icon: 'trash-gt'},
-		{
-			date: 'May 23',
-			description: 'Evento 3 con mas texto que el otro y con un poco mas',
-			title: 'Examination',
-			icon: 'units',
-		},
-		{
-			date: 'Feb 23',
-			description: 'Evento 4 con mas texto que el otro y con un poco mas del otro',
-			title: 'New patient',
-			icon: 'user',
-		},
-	];
 	return (
-		<div className="container">
-			<section>
-				<VerticalLine events={events} variant="secondary" />
-			</section>
+		<div className="view">
+			<Content />
 		</div>
 	);
 }
---
+++ docs/modules/components/vertical-line/ts/views/result.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {VerticalLine} from 'pragmate-ui/vertical-line';
+import {events} from './list-events';
+
+export function Result() {
+	return (
+		<div className="content">
+			<VerticalLine events={events} variant="secondary" />
+		</div>
+	);
+}
