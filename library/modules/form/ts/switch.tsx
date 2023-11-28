58357e38de56738d470bf431eb43372bd3009b70
Francisco Sanvicente Pico <64644109+FranciscoSanvicente@users.noreply.github.com>
2023-11-28 14:13:36 -0400

    Fix: bug in Switch: name was not being passed, styles corrected in textarea (#37)
    Co-authored-by: sanvicente.francisco <fsanvicente@balearesgroup.com>

--- library/modules/form/ts/switch.tsx
+++ library/modules/form/ts/switch.tsx
@@ -1,41 +1,46 @@
 import React from 'react';
-import { InputHTMLAttributes } from 'react';
-interface props extends InputHTMLAttributes<HTMLInputElement> {
+import { InputHTMLAttributes, ChangeEvent } from 'react';
+interface IProps extends InputHTMLAttributes<HTMLInputElement> {
 	variant?: string;
 	title?: string;
 }
 
-export /* bundle */ function Switch(props: props) {
-	const { checked, onChange, variant = 'primary', disabled, size = 'md', className, id, title, required } = props;
-	const [isChecked, setIsChecked] = React.useState(checked);
+export /* bundle */ function Switch(props: IProps): JSX.Element {
+	const { checked, onChange, variant = 'primary', disabled, size = 'md', className, } = props;
+	const [isChecked, setIsChecked] = React.useState<boolean>(checked);
 
-	const handleChange = event => {
+	const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+		event.stopPropagation()
 		setIsChecked(event.currentTarget.checked);
-		console.log(event);
 		onChange && onChange(event);
 	};
 
 	let classNameSwitch: string = `pui-element-switch ${className ? className : ''}`;
 	classNameSwitch += disabled ? ' disabled' : '';
 
-	let cls = isChecked ? `pui-element-switch__label active` : 'pui-element-switch__label';
+	let cls: string = isChecked ? `pui-element-switch__label active` : 'pui-element-switch__label';
 	cls += variant ? ` ${variant}` : '';
 	cls += size ? ` ${size}` : '';
 
-	let isId = !id ? `pui-element-switch` : id;
+	const properties: IProps = { ...props };
+
+	['className', 'checked', 'onChange', 'variant', 'size', "id",].forEach(prop => {
+		delete properties[prop];
+	});
+
+	const id = !!props.id ? props.id : props.name ?? "pui-element-switch"
+
 	return (
 		<div className={classNameSwitch}>
 			<input
 				className='pui-element-switch__checkbox'
-				id={isId}
-				title={title}
-				required={required}
+				id={id}
 				type='checkbox'
-				disabled={disabled}
 				checked={isChecked}
 				onChange={handleChange}
+				{...properties}
 			/>
-			<label className={cls} htmlFor={isId}>
+			<label className={cls} htmlFor={id}>
 				<span className={`label__btn--slider`} />
 			</label>
 		</div>
