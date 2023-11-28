28789ea0d4c1b64a3aca75cf8620155b453d60ab
Julio Rodriguez <jircdev@gmail.com>
2023-11-28 09:13:50 -0300

    docs: update docs removing unnecesary files

--- docs/modules/components/form/input/ts/views/index.tsx
+++ docs/modules/components/form/input/ts/views/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Content from './content.mdx';
-import {Code} from 'pragmate-ui/code';
+import { Code } from 'pragmate-ui/code';
 export /*bundle*/
 function View() {
 	return (
-		<div className="view">
+		<div className='view'>
 			<Content />
 		</div>
 	);
