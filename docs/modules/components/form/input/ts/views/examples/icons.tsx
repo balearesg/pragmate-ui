5e749748316a2c1f68afce1513625b92ffa7d212
sanvicente.francisco <fsanvicente@balearesgroup.com>
2023-12-12 07:44:50 -0400

    Fix: fixed icon input documentation

--- docs/modules/components/form/input/ts/views/examples/icons.tsx
+++ docs/modules/components/form/input/ts/views/examples/icons.tsx
@@ -5,9 +5,11 @@ import { Input } from 'pragmate-ui/form';
 export function Icons(): JSX.Element {
     return (
         <UIExample>
-            Icon (default on the right)
+            <h5>Icon (default on the right)</h5>
+            For this use the icon property with the icon name
             <Input label="this is a label" icon="user" />
-            Icon on the left
+            <h5>Icon on the left</h5>
+            For this pass an html class left-icon.
             <Input label="this is a label" icon="user" className="left-icon" />
         </UIExample>
     )
