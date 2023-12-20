47f4e40e319fb48e21efbae2cfa71a108089f8f1
sanvicente.francisco <fsanvicente@balearesgroup.com>
2023-12-20 15:47:50 -0400

    Fix: change class pragmate-icon for pui-icon

--- library/modules/common/icons/ts/icon.tsx
+++ library/modules/common/icons/ts/icon.tsx
@@ -9,36 +9,36 @@ interface props extends SVGAttributes<SVGSVGElement> {
 	icon?: any;
 }
 export /*bundle*/
-const Icon = (props: props): JSX.Element => {
-	let { icon, className } = props;
-	const iconsList: icon = PRAGMATE_ICONS;
+	const Icon = (props: props): JSX.Element => {
+		let { icon, className } = props;
+		const iconsList: icon = PRAGMATE_ICONS;
 
-	if (!icon) return <div key='preload' />;
+		if (!icon) return <div key='preload' />;
 
-	let viewBox: string = '0 0 24 24';
-	if (iconsList.hasOwnProperty(icon)) icon = iconsList[icon];
+		let viewBox: string = '0 0 24 24';
+		if (iconsList.hasOwnProperty(icon)) icon = iconsList[icon];
 
-	if (typeof icon === 'object') {
-		viewBox = icon.viewBox ? icon.viewBox : viewBox;
-		icon = icon.icon;
-	}
-
-	const properties: props = Object.assign(
-		{ ...props },
-		{
-			viewBox: props.viewBox ? props.viewBox : viewBox,
-			className: !className ? 'pragmate-icon' : `pragmate-icon ${className}`,
+		if (typeof icon === 'object') {
+			viewBox = icon.viewBox ? icon.viewBox : viewBox;
+			icon = icon.icon;
 		}
-	);
 
-	props.title ? (properties['data-tippy-content'] = props.title) : null;
-	delete properties.icon;
+		const properties: props = Object.assign(
+			{ ...props },
+			{
+				viewBox: props.viewBox ? props.viewBox : viewBox,
+				className: !className ? 'pui-icon' : `pui-icon ${className}`,
+			}
+		);
 
-	props['data-item'] ? (properties['data-item'] = props['data-item']) : null;
+		props.title ? (properties['data-tippy-content'] = props.title) : null;
+		delete properties.icon;
 
-	return (
-		<svg {...properties}>
-			<g dangerouslySetInnerHTML={{ __html: icon }} />
-		</svg>
-	);
-};
+		props['data-item'] ? (properties['data-item'] = props['data-item']) : null;
+
+		return (
+			<svg {...properties}>
+				<g dangerouslySetInnerHTML={{ __html: icon }} />
+			</svg>
+		);
+	};
