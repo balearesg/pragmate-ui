35c8889b42a48643372e05cfb76c5d339be20bb3
sanvicente.francisco <fsanvicente@balearesgroup.com>
2024-01-09 19:54:28 -0400

    Fix: default empty value in Input

--- library/modules/form/controls/ts/input/control.tsx
+++ library/modules/form/controls/ts/input/control.tsx
@@ -33,7 +33,7 @@ export function ControlSelector(): JSX.Element {
 	const attrs = {
 		id: id ?? name,
 		placeholder: placeholder ?? ' ',
-		value: value ?? ' '
+		value: value ?? ''
 	};
 	return (
 		<>
