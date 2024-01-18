aabe6baf871d5d47608787350714f16dc4b0700e
Julio Rodriguez <jircdev@gmail.com>
2024-01-18 11:41:18 -0300

    feat: Add support to pass clasName to tab container and tab component

--- library/modules/interaction/tabs/ts/index.tsx
+++ library/modules/interaction/tabs/ts/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import {TabsContext} from './context';
-import {ITabProps} from './interfaces';
+import { TabsContext } from './context';
+import { ITabProps } from './interfaces';
 
-export /* bundle  */ const TabsContainer: React.FC<ITabProps> = ({active, onChange, children, panes}) => {
+export /* bundle  */ const TabsContainer: React.FC<ITabProps> = ({ active, onChange, className, children, panes }) => {
 	const activeIndex = active || 0;
 	const [activeTab, setActiveTab] = React.useState(activeIndex);
-	const value = {panes, activeTab, setActiveTab, onChange};
+	const value = { panes, activeTab, setActiveTab, onChange };
 
+	const cls = `tab-container${className ? ` ${className}` : ''}`;
 	return (
 		<TabsContext.Provider value={value}>
-			<section className="tab-container">{children}</section>
+			<section className={cls}>{children}</section>
 		</TabsContext.Provider>
 	);
 };
