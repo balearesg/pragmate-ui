53d051e04914690a04560a417a5943987b21bc95
Julio Rodriguez <jircdev@gmail.com>
2024-01-31 14:20:29 -0300

    fix: Solve ref type

--- library/modules/common/icons/ts/icon.tsx
+++ library/modules/common/icons/ts/icon.tsx
@@ -4,43 +4,43 @@ interface icon {
 	icon?: string;
 	viewBox?: SVGAttributes<SVGSVGElement>['viewBox'];
 }
-interface props extends SVGAttributes<SVGSVGElement> {
+interface IProps extends SVGAttributes<SVGSVGElement> {
 	title?: string;
 	icon?: any;
-	ref?: Ref<HTMLButtonElement>
+	ref?: Ref<SVGSVGElement>;
 	className?: string;
 }
 export /*bundle*/
-	const Icon = (props: props): JSX.Element => {
-		let { icon, className } = props;
-		const iconsList: icon = PRAGMATE_ICONS;
+const Icon = (props: IProps): JSX.Element => {
+	let { icon, className } = props;
+	const iconsList: icon = PRAGMATE_ICONS;
 
-		if (!icon) return <div key='preload' />;
+	if (!icon) return <div key='preload' />;
 
-		let viewBox: string = '0 0 24 24';
-		if (iconsList.hasOwnProperty(icon)) icon = iconsList[icon];
+	let viewBox: string = '0 0 24 24';
+	if (iconsList.hasOwnProperty(icon)) icon = iconsList[icon];
 
-		if (typeof icon === 'object') {
-			viewBox = icon.viewBox ? icon.viewBox : viewBox;
-			icon = icon.icon;
+	if (typeof icon === 'object') {
+		viewBox = icon.viewBox ? icon.viewBox : viewBox;
+		icon = icon.icon;
+	}
+
+	const properties: IProps = Object.assign(
+		{ ...props },
+		{
+			viewBox: props.viewBox ? props.viewBox : viewBox,
+			className: !className ? 'pui-icon' : `pui-icon ${className}`,
 		}
+	);
 
-		const properties: props = Object.assign(
-			{ ...props },
-			{
-				viewBox: props.viewBox ? props.viewBox : viewBox,
-				className: !className ? 'pui-icon' : `pui-icon ${className}`,
-			}
-		);
+	props.title ? (properties['data-tippy-content'] = props.title) : null;
+	delete properties.icon;
 
-		props.title ? (properties['data-tippy-content'] = props.title) : null;
-		delete properties.icon;
+	props['data-item'] ? (properties['data-item'] = props['data-item']) : null;
 
-		props['data-item'] ? (properties['data-item'] = props['data-item']) : null;
-
-		return (
-			<svg {...properties}>
-				<g dangerouslySetInnerHTML={{ __html: icon }} />
-			</svg>
-		);
-	};
+	return (
+		<svg {...properties}>
+			<g dangerouslySetInnerHTML={{ __html: icon }} />
+		</svg>
+	);
+};
