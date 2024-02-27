a08ec0d65c103e2c9da2892415787449dc819c06
sanvicente.francisco <fsanvicente@balearesgroup.com>
2024-02-27 13:23:35 -0400

    Fix: solve key in groupCheckbox and not picker in disabled input

--- library/modules/form/controls/ts/input/components/date.tsx
+++ library/modules/form/controls/ts/input/components/date.tsx
@@ -9,13 +9,17 @@ export function Date(): JSX.Element {
 	const { input, props, isDate } = useInputContext();
 
 	if (!isDate) return null;
-	const icon = props.icon || "calendar"
+	const icon = props.icon || "calendar";
+	const showPicker = () => {
+		if (props.disabled) return;
+		(input.current as any).showPicker()
+	}
 	return (
 		<Icon
 			key='icon'
 			icon={icon}
 			className='pui-input__icon'
-			onClick={() => (input.current as any).showPicker()}
+			onClick={showPicker}
 		/>
 	);
 }
