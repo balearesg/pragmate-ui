06ca8a1f1c864009c631df01d4e0195cae76f674
gabojcb <gaboj0531@gmail.com>
2024-06-28 09:12:02 -0400

    Chore(project/base): remove properties innecesary

--- src/modules/base/ts/index.ts
+++ src/modules/base/ts/index.ts
@@ -1,13 +1,12 @@
 export /* bundle */ interface IPUIProps<T = unknown> extends React.InputHTMLAttributes<T> {
-	show?: boolean;
 	className?: string;
+	/**
+	* @deprecated 
+	*/ 
 	type?: string;
-	message?: string;
 	title?: string;
 	children?: React.ReactNode;
-    variant?: TVariant;
-    label?: string;
-    icon?: string;
+    variant?: Variant;
 }
 
-export type TVariant = 'primary' | 'secondary' | 'tertiary' | 'success' | 'info' | 'error' | 'warning';
+export type Variant = 'primary' | 'secondary' | 'tertiary' | 'success' | 'info' | 'error' | 'warning';
