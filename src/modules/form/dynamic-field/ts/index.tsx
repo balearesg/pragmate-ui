4e3c1abf337b3f0fa8e97ccebed4205642e031a9
Julio Rodriguez <jircdev@gmail.com>
2024-10-24 13:00:44 -0300

    chore: remove unused file

--- src/modules/form/dynamic-field/ts/index.tsx
+++
@@ -1,2 +0,0 @@
-export /*bundle*/ function DynamicFieldContainer({ children }: { children: React.ReactNode }) {
-	return <ModuleContext.Provide value={{ fields, add, remove, update }}>{children}</ModuleContext.Provide>;
