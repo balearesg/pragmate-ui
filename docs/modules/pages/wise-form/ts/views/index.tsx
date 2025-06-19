321cf1636a95fffbee65a10f983ea221827e94c0
Julio Rodriguez <jircdev@gmail.com>
2025-06-19 07:46:04 -0300

    docs: add wise-form wizard page (#68)

---
+++ docs/modules/pages/wise-form/ts/views/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Docs from './es/index.mdx';
+
+export /*bundle*/ function View() {
+  return (
+    <div className="page__container">
+      <Docs />
+    </div>
+  );
+}
